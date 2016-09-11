178d0f01605f5dbc859c16d2c164893081dba03b
shoko furushita <shktkt219@gmail.com>
2016-09-11 14:04:03 -0400

    fixed nested form bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   has_many :post_tags
   has_many :tags, through: :post_tags
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
   def tags_attributes=(tag_attributes)
     tag_attributes.values.each do |tag_attribute|
